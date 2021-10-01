cc2a599d57d0bf36d820ad44777ab2a0a0af2e94
artjomsR1 <82141804+artjomsR1@users.noreply.github.com>
2021-10-01 10:03:41 +0300

    vievads par vardiem un funkcijam

--- programma1.py
+++ programma1.py
@@ -4,3 +4,16 @@ skaitlis=5
 vaardi="skola"
 print(skaitlis)
 print(vaardi)
+
+v1, v2, v3 = "as", "sd", "df" #dazados mainigos var definet viena rina
+print(v1, v2, v3)
+
+m1=m2=m3="divi vardi"#definejam mainigos lai tie butu vienadi
+print(m1, m2, m3)
+
+def nosaukumi(name):
+    print("Sveiki visai pasaulei", name)
+
+nosaukumi("Es")
+nosaukumi("Kazahs")
+nosaukumi("Čečenis")#drfinejam funkcuju
