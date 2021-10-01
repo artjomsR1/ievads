8edfe9b32c118e856577611c1e46ff3e9d799c40
artjomsR1 <82141804+artjomsR1@users.noreply.github.com>
2021-10-01 10:30:57 +0300

    as

---
+++ 1okt_otra_stunda.py
@@ -0,0 +1,21 @@
+sk1=13
+sk2=3
+
+sask=sk1+sk2#saskaitam rez=16
+print(sask)
+print(sk1,"+",sk2,"=",sask)
+
+at=sk1-sk2#atnemsana rez=10
+print(at)
+
+reiz=sk1*sk2#reizinasana rez=39
+print(reiz)
+
+dal=sk1/sk2#dalisana rez=4.3(3)
+print(dal)
+
+dal2=sk1//sk2#dalisana bez atlikuma rez=4
+print(dal2)
+
+atl=sk1%sk2#atlikuma noteiksana rez=1
+print(atl)
